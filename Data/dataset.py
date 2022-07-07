24e12bf0e6b60935740e3a914c66f91b32796c28
Ryzon3 <ryanmcole20@gmail.com>
2022-07-07 15:13:39 -0400

    Fixed data for scheduler

--- Data/dataset.py
+++ Data/dataset.py
@@ -13,12 +13,12 @@ def importCourses(courseList):
     return output
 
 # generates a random dataset of students
-# each student can take up to 8 courses with a minimum of 5
-# for each empty slot a study hall is inserted
+# each student can take up to 8 courses
+# study halls are more heavily weighted 
 def generateStudents(amount, courses):
     names = open("us_names.txt", "r")
     lines = names.readlines()
-    students = []
+    students = {}
 
     for i in range(amount):
         student = {}
@@ -29,15 +29,31 @@ def generateStudents(amount, courses):
             for clazz in student["classes"]:
                 subjs.append(clazz[0])
             clazz = choice(courses)
-            print(clazz[0])
             while clazz[0] in subjs and not(clazz[0] == 'Z'):
                 clazz = choice(courses)
             student["classes"].append(clazz)
-        students.append(students)
+        classes = student["classes"]
+        fixedClasses = []
+        for clazz in classes:
+            fixedClasses.append(clazz[1])
+        student["classes"] = fixedClasses
+        students[student["name"]] = student
     return students
 
 # main function
 # generates the csv files
 if __name__ == "__main__":
     courses = importCourses("course_list.txt")
-    students = generateStudents(1000, courses)
+    students = generateStudents(10, courses)
+
+    f = open('students.csv', 'w')
+    # write header to CSV
+    f.write("Name,Class 1,Class 2,Class 3,Class 4,Class 5,Class 6,Class 7,Class 8\n")
+    for student in students:
+        txt = students[student]["name"]
+        for clazz in students[student]["classes"]:
+            txt = txt + ","
+            txt = txt + clazz
+        txt = txt + "\n"
+        f.write(txt)
+    f.close()
